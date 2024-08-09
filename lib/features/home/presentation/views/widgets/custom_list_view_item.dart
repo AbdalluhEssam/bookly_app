2d32aeedc215fa627bb578595a7a011a56d2c7f7
abdal <abdallhesam100@gmail.com>
2024-08-10 00:18:41 +0300

    add listview item

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2 / 3,
+      child: Container(
+        clipBehavior: Clip.antiAliasWithSaveLayer,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          image: const DecorationImage(
+              image: AssetImage(AssetsApp.bookImage),
+              fit: BoxFit.cover,
+              alignment: Alignment.centerLeft),
+          borderRadius: BorderRadius.circular(16),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'custom_app_bar.dart';
+import 'custom_list_view_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -10,9 +13,28 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomApBar(),
-
-
+        FeaturedBooksListView(),
       ],
+    );
+  }
+}
+
+class FeaturedBooksListView extends StatelessWidget {
+  const FeaturedBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          clipBehavior: Clip.none,
+          itemBuilder: (context, index) => const FeaturedListViewItem(),
+          separatorBuilder: (context, index) => const SizedBox(
+                width: 12,
+              ),
+          itemCount: 5),
     );
   }
 }
