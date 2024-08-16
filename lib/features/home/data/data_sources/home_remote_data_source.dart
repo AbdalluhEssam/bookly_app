1583fd2c8c5901ff5fc40b5336c9661e60b687e6
abdal <abdallhesam100@gmail.com>
2024-08-16 22:34:54 +0300

    Cache Featured Books

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,6 +1,7 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/core/utils/function/save_books.dart';
 import 'package:bookly_app/features/home/data/models/book_model.dart';
-
 import '../../domain/entities/book_entity.dart';
 
 abstract class HomeRemoteDataSource {
@@ -20,13 +21,15 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
         endPoint: "volumes?q=subject:programming&Filtering=free-ebooks");
 
     List<BookEntity> books = getBookList(data);
+    saveBooksData(books, kFeaturedBooks);
     return books;
   }
 
   @override
   Future<List<BookEntity>> fetchNewestBooks() async {
     var data = await apiService.get(
-        endPoint: "volumes?q=computer science&Sorting=newest&Filtering=free-ebooks");
+        endPoint:
+            "volumes?q=computer science&Sorting=newest&Filtering=free-ebooks");
 
     List<BookEntity> books = getBookList(data);
     return books;
